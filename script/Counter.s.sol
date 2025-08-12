4c1228ed405331a7f682a19a1028d9ff8e6bc225
Sinoo <khoasinno@gmail.com>
2025-08-12 18:16:55 +0700

    update PayrollTreasury file

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {PayrollTreasury} from "../src/PayrollTreasury.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract PayrollTreasuryScript is Script {
+    PayrollTreasury public payrollTreasury;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        payrollTreasury = new PayrollTreasury();
 
         vm.stopBroadcast();
     }
